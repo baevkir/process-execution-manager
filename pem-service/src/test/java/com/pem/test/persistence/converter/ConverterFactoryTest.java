2c0d08c4daccfcbc6206a74f260fe8ab12de6827
baevkir <baevkir@gmail.com>
2016-11-13 14:28:23 +0200

    Add converter factory tests

--- pem-service/src/test/java/com/pem/test/persistence/converter/ConverterFactoryTest.java
+++ pem-service/src/test/java/com/pem/test/persistence/converter/ConverterFactoryTest.java
@@ -4,10 +4,16 @@ import com.pem.operation.basic.Operation;
 import com.pem.operation.composite.SyncCompositeOperation;
 import com.pem.operation.condition.BinaryConditionOperation;
 import com.pem.operation.condition.IntegerConditionOperation;
+import com.pem.operation.loop.condition.DoWhileOperation;
+import com.pem.operation.loop.condition.WhileOperation;
+import com.pem.operation.loop.counter.CounterLoopOperation;
 import com.pem.persistence.converter.ConverterFactory;
 import com.pem.persistence.model.operation.common.OperationEntity;
 import com.pem.persistence.model.operation.composite.SyncCompositeOperationEntity;
 import com.pem.persistence.model.operation.condition.IntegerConditionOperationEntity;
+import com.pem.persistence.model.operation.loop.CounterLoopOperationEntity;
+import com.pem.persistence.model.operation.loop.condition.DoWhileLoopOperationEntity;
+import com.pem.persistence.model.operation.loop.condition.WhileLoopOperationEntity;
 import com.pem.test.common.TestEntityCreator;
 import com.pem.test.common.config.TestConfig;
 import org.junit.Assert;
@@ -55,5 +61,35 @@ public class ConverterFactoryTest {
         Operation operation = converterFactory.convert(operationEntity, Operation.class);
         Assert.assertNotNull(operation);
         Assert.assertTrue(operation instanceof IntegerConditionOperation);
+    }
+
+    @Test
+    public void testCounterLoopOperationConverter() {
+        CounterLoopOperationEntity operationEntity = new CounterLoopOperationEntity();
+        operationEntity.setCount(100);
+        operationEntity.setOperation(creator.createIntegerConditionOperationEntity());
+        Operation operation = converterFactory.convert(operationEntity, Operation.class);
+        Assert.assertNotNull(operation);
+        Assert.assertTrue(operation instanceof CounterLoopOperation);
+    }
+
+    @Test
+    public void testDoWhileOperationConverter() {
+        DoWhileLoopOperationEntity operationEntity = new DoWhileLoopOperationEntity();
+        operationEntity.setOperation(creator.createSyncCompositeOperationEntity());
+        operationEntity.setCalculator(creator.createBinaryCalculator());
+        Operation operation = converterFactory.convert(operationEntity, Operation.class);
+        Assert.assertNotNull(operation);
+        Assert.assertTrue(operation instanceof DoWhileOperation);
+    }
+
+    @Test
+    public void testWhileOperationConverter() {
+        WhileLoopOperationEntity operationEntity = new WhileLoopOperationEntity();
+        operationEntity.setOperation(creator.createIntegerConditionOperationEntity());
+        operationEntity.setCalculator(creator.createBinaryCalculator());
+        Operation operation = converterFactory.convert(operationEntity, Operation.class);
+        Assert.assertNotNull(operation);
+        Assert.assertTrue(operation instanceof WhileOperation);
     }
 }
