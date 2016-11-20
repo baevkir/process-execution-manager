4d8a92eeb400a157201cfdb450ceaa56ad60a41d
baevkir <baevkir@gmail.com>
2016-11-20 13:29:38 +0200

    add execution processor service

--- pem-service/src/test/java/com/pem/test/persistence/converter/ConverterFactoryTest.java
+++ pem-service/src/test/java/com/pem/test/persistence/converter/ConverterFactoryTest.java
@@ -24,8 +24,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.math.BigInteger;
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TestConfig.class)
 public class ConverterFactoryTest {
