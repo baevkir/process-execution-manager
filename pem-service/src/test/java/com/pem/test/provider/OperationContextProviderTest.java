dca7e7565e4609b4b038ed19c5bca796480dfb6b
baevkir <baevkir@gmail.com>
2016-11-20 21:01:47 +0200

    add test for OperationContextProvider

---
+++ pem-service/src/test/java/com/pem/test/provider/OperationContextProviderTest.java
@@ -0,0 +1,27 @@
+package com.pem.test.provider;
+
+import com.pem.common.provider.context.OperationContextProvider;
+import com.pem.common.provider.operation.OperationProvider;
+import com.pem.context.OperationContext;
+import com.pem.test.common.config.TestConfig;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = TestConfig.class)
+public class OperationContextProviderTest {
+
+    @Autowired
+    private OperationContextProvider contextProvider;
+
+    @Test
+    public void testCreateOperationContext() {
+        OperationContext context = contextProvider.createContext();
+        OperationContext context1 = contextProvider.createContext();
+        Assert.assertTrue(context != context1);
+    }
+}
